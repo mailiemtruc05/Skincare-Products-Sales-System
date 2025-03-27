2108a96f429731d46724a2230ea88acf8fc1d350
Lê Trung Đại <dailt20122004@gmail.com>
2025-03-27 20:41:08 +0700

    update product

---
+++ models/Product.java
@@ -0,0 +1,107 @@
+package edu.uth.wed_san_pham_cham_soc_da.models;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Nationalized;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ProductID", nullable = false)
+    private Integer id;
+
+    @Nationalized
+    @Column(name = "ProductName", nullable = false)
+    private String productName;
+
+    @Nationalized
+    @Lob
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Nationalized
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "cid", nullable = false)
+    private Category cid;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "sub_category_id", nullable = false) // Liên kết với SubCategory
+    private SubCategory subCategory;
+
+
+    @ColumnDefault("getdate()")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("getdate()")
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
