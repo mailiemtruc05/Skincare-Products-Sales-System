11a48965883d98810a3595523d03e83a3f19695d
hophatdat0405 <hodat80965734@gmail.com>
2025-03-29 09:09:15 +0700

    update file để hiển thị danh mục sản phẩm

--- Controllers/HomeController.java
+++ Controllers/HomeController.java
@@ -1,16 +1,34 @@
 package edu.uth.wed_san_pham_cham_soc_da.Controllers;
 
-import ch.qos.logback.core.model.Model;
+import edu.uth.wed_san_pham_cham_soc_da.Service.CategoryService;
+import edu.uth.wed_san_pham_cham_soc_da.models.Category;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 @Controller
 public class HomeController {
     @RequestMapping("/home")
     public String home() {
         return "index"; // Điều hướng đến templates/index.html
+    }
+
+    private final CategoryService categoryService;
+
+    public HomeController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+        return "index";
     }
 }
 
