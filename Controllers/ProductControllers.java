20a729b4bfa30d27ec3146051bde4110e7084bc9
Mai Liêm Trực <trucml0017@ut.edu.vn>
2025-03-31 21:47:09 +0700

    update productControllers

--- Controllers/ProductControllers.java
+++ Controllers/ProductControllers.java
@@ -1,12 +1,23 @@
 package edu.uth.wed_san_pham_cham_soc_da.Controllers;
 
+
+import edu.uth.wed_san_pham_cham_soc_da.Service.ProductService;
+import edu.uth.wed_san_pham_cham_soc_da.models.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductControllers {
     @RequestMapping("/product")
-    public String product() {
-        return "product"; // Điều hướng đến templates/product.html
-    }
+    public String product(Model model) {
+        model.addAttribute("LISTPRODUCT", service.getAll());
+        return "product";
+    } //tra ve trang product.html
+
+    @Autowired
+    ProductService service;
 }
