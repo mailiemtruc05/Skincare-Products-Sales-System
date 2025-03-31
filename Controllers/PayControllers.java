00870eddd1a9fbed526a5d94f364b5f5967f1fbc
Lê Trung Đại <dailt20122004@gmail.com>
2025-03-31 21:56:53 +0700

    update PayControllers

--- Controllers/PayControllers.java
+++ Controllers/PayControllers.java
@@ -1,10 +1,30 @@
 package edu.uth.wed_san_pham_cham_soc_da.Controllers;
+
+import edu.uth.wed_san_pham_cham_soc_da.Service.ShoppingCartService;
+import edu.uth.wed_san_pham_cham_soc_da.models.ShoppingCart;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
 @Controller
 public class PayControllers {
-    @RequestMapping("/pay")
-    public String pay() {
-        return "pay"; // Điều hướng đến templates/about.html
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @GetMapping("/pay")
+    public String pay(Model model) {
+        // Lấy danh sách sản phẩm trong giỏ hàng
+        Collection<ShoppingCart> cartItems = shoppingCartService.getAllItems();
+        // Tính tổng tiền
+        double totalPrice = shoppingCartService.getTotal();
+
+        // Đưa dữ liệu vào model để hiển thị trong giao diện
+        model.addAttribute("cartItems", cartItems);
+        model.addAttribute("totalPrice", totalPrice);
+
+        return "pay"; // Trả về trang thanh toán (pay.html)
     }
 }
