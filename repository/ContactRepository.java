3e2963a05af71e401847c6715eeaea4eb0cff021
TinTran1615 <duymapcuibap@gmail.com>
2025-03-31 21:36:31 +0700

    Add ContactRepository.java

---
+++ repository/ContactRepository.java
@@ -0,0 +1,7 @@
+package edu.uth.wed_san_pham_cham_soc_da.repository;
+import edu.uth.wed_san_pham_cham_soc_da.models.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+
+}
