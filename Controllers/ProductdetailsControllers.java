6ed35cf076a5a5bb848dd4d227f3ce3ab2382dce
hophatdat0405 <hodat80965734@gmail.com>
2025-04-04 22:43:41 +0700

    Update hiển thị thông tin sản phẩm chính xác

--- Controllers/ProductdetailsControllers.java
+++ Controllers/ProductdetailsControllers.java
@@ -1,10 +1,35 @@
 package edu.uth.wed_san_pham_cham_soc_da.Controllers;
+
+import edu.uth.wed_san_pham_cham_soc_da.models.Product;
+import edu.uth.wed_san_pham_cham_soc_da.Service.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+
 @Controller
 public class ProductdetailsControllers {
-    @RequestMapping("/product-details")
-    public String pay() {
-        return "product-details"; // Điều hướng đến templates/about.html
+    private final ProductService productService;
+
+    public ProductdetailsControllers(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/product-details/{id}")
+    public String productDetails(@PathVariable Integer id, Model model) {
+        Product product = productService.getProductById(id);
+        if (product == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Sản phẩm không tồn tại");
+        }
+
+        // Xử lý xuống dòng thành <br/>
+        String formattedDescription = product.getDescription().replace("\n", "<br/>");
+
+        model.addAttribute("product", product);
+        model.addAttribute("formattedDescription", formattedDescription); // Truyền biến đã xử lý
+
+        return "product-details";
     }
 }
