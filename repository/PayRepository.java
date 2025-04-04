49012bce8efb733f894db681326746730ca1059f
Lê Trung Đại <dailt20122004@gmail.com>
2025-04-04 22:40:27 +0700

    add PayRepository

---
+++ repository/PayRepository.java
@@ -0,0 +1,6 @@
+package edu.uth.wed_san_pham_cham_soc_da.repository;
+import edu.uth.wed_san_pham_cham_soc_da.models.Pay;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PayRepository extends JpaRepository<Pay, String> {
+}
