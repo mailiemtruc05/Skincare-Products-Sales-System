9be53a89b2a61d71cc356f0c787b9c590876ff21
hophatdat0405 <hodat80965734@gmail.com>
2025-04-16 18:51:48 +0700

    Update file ShoppingCartRepository.java

--- repository/ShoppingCartRepository.java
+++ repository/ShoppingCartRepository.java
@@ -1,7 +1,10 @@
 package edu.uth.wed_san_pham_cham_soc_da.repository;
+
 import edu.uth.wed_san_pham_cham_soc_da.models.ShoppingCart;
+import edu.uth.wed_san_pham_cham_soc_da.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
+    List<ShoppingCart> findByAccount(Account account);
 }
-
