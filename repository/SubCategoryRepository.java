10570a7e3ecb42d024c98b6e57433637c21e77ec
Phạm Minh Vinh Quang <quangpmv2507@gmail.com>
2025-04-21 12:42:51 +0700

    update CategoryRepository (hoàn chỉnh)

--- repository/SubCategoryRepository.java
+++ repository/SubCategoryRepository.java
@@ -1,7 +1,10 @@
 package edu.uth.wed_san_pham_cham_soc_da.repository;
+
 import edu.uth.wed_san_pham_cham_soc_da.models.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Integer> {
 }
 
