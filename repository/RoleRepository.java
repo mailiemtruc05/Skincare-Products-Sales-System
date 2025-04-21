5ebe2dc990c31df5cdc9f44122f63a4c35f00bba
Lê Trung Đại <dailt20122004@gmail.com>
2025-04-21 12:50:16 +0700

    update RoleRepository (hoàn chỉnh)

--- repository/RoleRepository.java
+++ repository/RoleRepository.java
@@ -4,7 +4,11 @@ import edu.uth.wed_san_pham_cham_soc_da.models.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Roles, Integer> {
     Roles findByName(String name);
+
+    Optional<Roles> findRolesByName(String name);
 }
