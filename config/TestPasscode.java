0fb471d7c1a99130cf9bbb09e18af8e4609372de
Mai Liêm Trực <mailiemtruc05@gmail.com>
2025-04-23 20:01:12 +0700

    Delete config/TestPasscode.java

--- config/TestPasscode.java
+++
@@ -1,9 +0,0 @@
-package edu.uth.wed_san_pham_cham_soc_da.config;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class TestPasscode {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("3456"));
-    }
-}
