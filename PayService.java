1d898cbeb3744b240d648ad6efc50373e20a5c81
Mai Liêm Trực <mailiemtruc05@gmail.com>
2025-04-24 23:03:11 +0700

    update PayService (thêm tính năng mã giảm giá)

---
+++ PayService.java
@@ -0,0 +1,12 @@
+package edu.uth.wed_san_pham_cham_soc_da.Service;
+
+import edu.uth.wed_san_pham_cham_soc_da.models.Account;
+import edu.uth.wed_san_pham_cham_soc_da.models.Pay;
+import edu.uth.wed_san_pham_cham_soc_da.models.ShoppingCart;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface PayService {
+    Pay processPayment(Account account, Pay payForm, List<ShoppingCart> cartItems, BigDecimal discount);
+}
